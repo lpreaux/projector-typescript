6fb97f98462a4a172313027247db97c89e377800
lpreaux <lpreaux@diginamic-formation.fr>
2024-02-21 14:32:35 +0100

    feat(config): Handling configuration and arguments verification

---
+++ src/config.ts
@@ -0,0 +1,91 @@
+import path from "path";
+import {Opts} from "./opt";
+
+export enum Operation {
+    Print,
+    Add,
+    Remove
+}
+
+export type Config = {
+    args: string[],
+    operation: Operation,
+    config: string,
+    pwd: string,
+}
+
+function getPwd(opts: Opts): string {
+    if (opts.pwd) {
+        return opts.pwd;
+    }
+    return process.cwd();
+}
+
+function getConfig(opts: Opts): string {
+    if (opts.config) {
+        return opts.config;
+    }
+    
+    const homeDir = process.env["HOME"];
+    const ConfigDir = process.env["XDG_CONFIG_HOME"];
+    if (!ConfigDir) {
+        throw new Error("unable to determine config location");
+    }
+    
+    if (ConfigDir === homeDir) {
+        return path.join(xdgConfigDir, ".projector.json");
+    }
+    
+    return path.join(ConfigDir, "projector", "config.json");
+}
+
+function getOperation(opts: Opts): Operation {
+    if (!opts.args | opts.args.length === 0) {
+        return [];
+    }
+
+    if (opts[0] === "add") {
+        return Operation.Add;
+    }
+
+    if (opts[0] === "remove") {
+        return Operation.Remove;
+    }
+
+    return Operation.Print;
+}
+
+function getArgs(opts: Opts): string[] {
+    if (!opts.args | opts.args.length === 0) {
+        return [];
+    }
+    
+    const operation = getOperation(opts);
+    if (operaiton === Operation.Print) {
+        if (opt.args.length > 1) {
+            throw new Error(`expected 0 or 1 arguments but got ${opts.args.length -1}`);
+        }
+        return opts.args;
+    }
+    
+    if (operation === Operation.Add) {
+        if (opts.args.length !== 3) {
+            throw new Error(`expected 2 arguments but got ${opts.args.lenght - 1}`);
+        }
+        return opts.args.slice(1);
+    }
+    
+    if (opts.args.length !== 2) {
+        throw new Error(`expected 1 arguments but got ${opts.args.lenght - 1}`);
+    }
+    return opts.args.slice(1);
+}
+
+export default function config(opts: Opts): Config {
+    return {
+        pwd: getPwd(opts),
+        config: getConfig(opts),
+        args: getArgs(opts),
+        operation: getOperation(opts),
+    }
+}
